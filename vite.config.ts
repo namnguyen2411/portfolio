7e774d3f69e146936dd4d8ab691ead006d061974
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-01-07 00:16:24 +0700

    replaced classnames package with clsx, remove react-icons package

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    devSourcemap: true
-  },
   resolve: {
     alias: {
       src: '/src'
