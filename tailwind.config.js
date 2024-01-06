22e31ab440f5bb9e4fc50f1d613661742d13dd3d
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-01-07 00:33:41 +0700

    added fonts, color, customize container class and create light/dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
+const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
+  content: ['index.html', './src/**/*.{js,ts,jsx,tsx,css}'],
+  darkMode: 'class',
+  corePlugins: {
+    container: false
   },
-  plugins: [],
-};
+  theme: {
+    extend: {
+      fontFamily: {
+        openSans: ['Open Sans', 'sans-serif'],
+        playfair: ['Playfair Display', 'Open Sans', 'sans-serif']
+      },
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        bgTheme: 'rgb(var(--color-background) / <alpha-value>)',
+        textTheme: 'rgb(var(--color-text) / <alpha-value>)',
+        navBgTheme: 'rgb(var(--color-nav-background) / <alpha-value>)',
+        navTextTheme: 'rgb(var(--color-nav-text) / <alpha-value>)'
+      }
+    }
+  },
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '1280px',
+          marginLeft: 'auto',
+          marginRight: 'auto'
+        }
+      })
+    })
+  ]
+}
