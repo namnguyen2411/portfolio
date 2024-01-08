ccec244b0ace90d05d0a3ca45f8fe23d57dcc136
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-01-08 11:07:39 +0700

    Added images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,39 @@
+import header from './header.png'
+import about from './about.jpg'
+import shopeeClone from './shopeeClone.jpg'
+import project2 from './project2.jpg'
+import hangman from './hangman.jpg'
+import html from './html.png'
+import css from './css.png'
+import javascript from './javascript.png'
+import react from './react.png'
+import tailwindcss from './tailwindcss.png'
+import redux from './redux.png'
+import reactquery from './reactquery.svg'
+import emailGif from './email.gif'
+import phoneGif from './phone.gif'
+import typescript from './typescript.png'
+import nodejs from './nodejs.png'
+import expressjs from './expressjs.png'
+import mongodb from './mongodb.png'
+
+export {
+  header,
+  about,
+  shopeeClone,
+  project2,
+  hangman,
+  html,
+  css,
+  javascript,
+  react,
+  tailwindcss,
+  redux,
+  reactquery,
+  emailGif,
+  phoneGif,
+  typescript,
+  nodejs,
+  expressjs,
+  mongodb
+}
