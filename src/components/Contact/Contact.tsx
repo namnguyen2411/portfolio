fe8ecef24ae35f1a156e2844ca4f699ef9f132b0
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-01-15 08:08:24 +0700

    Implement Contact component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,153 @@
+import { useState, useRef, FormEvent } from 'react'
+import { m, LazyMotion, domAnimation } from 'framer-motion'
+import emailjs from '@emailjs/browser'
+import Lottie, { type LottieRefCurrentProps } from 'lottie-react'
+
+import SectionHeadings from '../SectionHeadings'
+import ClickableComponent from '../ClickableComponent'
+import { emailGif, emailSentAnimation } from 'src/assets/images'
+import { FormDataType } from 'src/types/form.type'
+import ContactForm from './ContactForm'
+
+const INITIAL_POSITION = '14%'
+
+const initialFormData = {
+  name: '',
+  email: '',
+  message: ''
+}
+
+const variants = {
+  details: {
+    initial: { opacity: 0, x: `-${INITIAL_POSITION}` },
+    whileInView: { opacity: 1, x: 0, transition: { duration: 1 } }
+  },
+  form: {
+    initial: { opacity: 0, x: INITIAL_POSITION },
+    whileInView: { opacity: 1, x: 0, transition: { duration: 1 } }
+  },
+  popup: {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 }
+  }
+}
+
 export default function Contact() {
+  const [formData, setFormData] = useState<FormDataType>(initialFormData)
+  const [isEmailSent, setIsEmailSent] = useState(false)
+  const [isEmailError, setIsEmailError] = useState(false)
+
+  const emailAnimationRef = useRef<LottieRefCurrentProps>(null)
+  const formContainerRef = useRef<HTMLDivElement>(null)
+  const formRef = useRef<HTMLFormElement>(null)
+  const contactRef = useRef<HTMLDivElement>(null)
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      if (!formRef.current) return
+      // eslint-disable-next-line import/no-named-as-default-member
+      const result = await emailjs.sendForm(
+        import.meta.env.VITE_EMAIL_SERVICE_ID as string,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID as string,
+        formRef.current,
+        import.meta.env.VITE_EMAIL_PUB_KEY as string
+      )
+      if (result.status === 200) {
+        setIsEmailSent(true)
+      }
+    } catch (err) {
+      setIsEmailError(true)
+      console.log(err)
+    }
+
+    setFormData(initialFormData)
+  }
+
   return (
-    <section id="contact" className="min-h-screen">
-      Contact
-    </section>
+    <>
+      <div className="container px-2 pt-2">
+        <SectionHeadings heading3={'Take A Coffee &'} heading2={'Chat With Me'} />
+        <div className="mt-20 grid grid-cols-1 gap-10 lg:grid-cols-2">
+          {/* Contact details */}
+          <div className="col-span-1 lg:text-left">
+            <LazyMotion features={domAnimation}>
+              <m.div
+                ref={contactRef}
+                initial={'initial'}
+                whileInView={'whileInView'}
+                viewport={{ once: true, amount: 0.1 }}
+                variants={variants.details}
+              >
+                <ClickableComponent
+                  href="mailto:phuongnam.nguyen241196@gmail.com"
+                  className="mx-auto flex w-fit items-center gap-4 bg-navBgTheme px-4 py-2 shadow-md shadow-neutral-400 duration-200 hover:text-primary lg:mx-0"
+                >
+                  <div className="w-[60px]">
+                    <img src={emailGif} className="w-full object-cover" />
+                  </div>
+                  <div>
+                    phuongnam.nguyen241196
+                    <span className="block text-center md:hidden">@gmail.com</span>
+                  </div>
+                </ClickableComponent>
+              </m.div>
+            </LazyMotion>
+
+            {/* Social Network */}
+          </div>
+          {/* Contact form */}
+          <LazyMotion features={domAnimation}>
+            <m.div
+              ref={formContainerRef}
+              initial={'initial'}
+              whileInView={'whileInView'}
+              viewport={{ once: true, amount: 0.3 }}
+              variants={variants.form}
+            >
+              <ContactForm formData={formData} setFormData={setFormData} handleSubmit={handleSubmit} ref={formRef} />
+            </m.div>
+          </LazyMotion>
+        </div>
+      </div>
+
+      {isEmailSent && (
+        <LazyMotion features={domAnimation}>
+          <m.div
+            className="fixed inset-0 z-30 bg-black/20 backdrop-blur-md"
+            initial={'initial'}
+            animate={'animate'}
+            variants={variants.popup}
+          >
+            <div className="absolute left-1/2 top-[40%] flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center md:w-[412px]">
+              <Lottie
+                className="aspect-square w-[212px] md:w-[312px]"
+                lottieRef={emailAnimationRef}
+                animationData={emailSentAnimation}
+                loop={false}
+                initialSegment={[0, 67]}
+                onPlay={() => {
+                  emailAnimationRef.current?.setSpeed(0.1)
+                }}
+              />
+              <m.div
+                className="text-center text-red-600"
+                transition={{ duration: 0.25, delay: 0.75 }}
+                variants={variants.popup}
+                onAnimationComplete={() => {
+                  setTimeout(() => {
+                    setIsEmailSent(false)
+                  }, 4000)
+                }}
+              >
+                <p className="text-base md:text-xl">Email sent</p>
+                <p className="text-base md:text-xl">I will get back to you as soon as possible</p>
+              </m.div>
+            </div>
+          </m.div>
+        </LazyMotion>
+      )}
+    </>
   )
 }
---
+++ src/components/Contact/ContactForm.tsx
@@ -0,0 +1,105 @@
+import { FormEvent, FormHTMLAttributes, forwardRef } from 'react'
+import ClickableComponent from '../ClickableComponent'
+import { FormDataType } from 'src/types/form.type'
+
+type ContactFormProps = {
+  formData: FormDataType
+  setFormData: React.Dispatch<React.SetStateAction<FormDataType>>
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void>
+} & FormHTMLAttributes<HTMLFormElement>
+
+const ContactForm = forwardRef<HTMLFormElement, ContactFormProps>(function ContactFormInner(
+  { formData, setFormData, handleSubmit },
+  ref
+) {
+  const { name, email, message } = formData
+
+  return (
+    <form
+      ref={ref}
+      spellCheck="false"
+      noValidate
+      onSubmit={(e) => {
+        void handleSubmit(e)
+      }}
+      className="neumorphism col-span-1 mx-auto grid min-w-[85%] gap-8 rounded-lg bg-navBgTheme px-10 py-5 lg:mx-0 lg:max-w-none"
+    >
+      <span className="text-center text-3xl font-medium">Let&apos;s work together</span>
+      {/* Email */}
+      <div>
+        <div className="mb-6 flex">
+          <label
+            htmlFor="user_email"
+            className="flex h-12 w-[52px] items-center justify-center overflow-hidden rounded-bl-md rounded-tl-md border-2 border-r-0 border-gray-300 bg-gray-200 dark:border-gray-600 dark:bg-[#4b5563]"
+          >
+            <svg
+              className="h-4 w-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 16"
+            >
+              <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+              <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+            </svg>
+          </label>
+          <input
+            type="text"
+            name="user_email"
+            id="user_email"
+            value={email}
+            onChange={(e) => setFormData((prev) => ({ ...prev, email: e.target.value }))}
+            className="input-neumorphism w-full rounded-br-lg rounded-tr-lg border-2 border-gray-300 bg-bgTheme p-2.5 text-textTheme transition-colors focus:border-primary focus:outline-none dark:border-gray-600 dark:focus:border-primary dark:focus:outline-none"
+            placeholder="Your Email"
+          />
+        </div>
+        {/* Name */}
+        <div className="flex">
+          <label
+            htmlFor="user_name"
+            className="inline-flex aspect-square w-12 items-center justify-center rounded-s-md border-2 border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400"
+          >
+            <svg
+              className="h-4 w-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+            </svg>
+          </label>
+          <input
+            type="text"
+            name="user_name"
+            id="user_name"
+            value={name}
+            onChange={(e) => setFormData((prev) => ({ ...prev, name: e.target.value }))}
+            className="input-neumorphism w-full rounded-br-lg rounded-tr-lg border-2 border-gray-300 bg-bgTheme p-2.5 text-textTheme transition-colors focus:border-primary focus:outline-none dark:border-gray-600 dark:focus:border-primary dark:focus:outline-none"
+            placeholder="Your Name"
+          />
+        </div>
+      </div>
+      {/*  */}
+      <textarea
+        value={message}
+        onChange={(e) => setFormData((prev) => ({ ...prev, message: e.target.value }))}
+        name="message"
+        placeholder="Your message..."
+        rows={6}
+        minLength={3}
+        // className="input px-4 py-3 ring-0 focus:outline-primary dark:bg-[#374151]"
+        className="input-neumorphism rounded-lg border-2 border-gray-300 bg-bgTheme p-2.5 text-textTheme transition-colors focus:border-primary focus:outline-none dark:border-gray-600 dark:focus:border-primary dark:focus:outline-none"
+      />
+      <ClickableComponent
+        type="submit"
+        className="mx-auto w-fit rounded-lg bg-secondary px-5 py-2 text-white shadow-md shadow-neutral-500 duration-200 hover:animate-wave disabled:pointer-events-none disabled:opacity-50"
+        disabled={name === '' || email.length <= 6}
+      >
+        Send
+      </ClickableComponent>
+    </form>
+  )
+})
+
+export default ContactForm
