adff192168cc4f371325bcdb5d5cd289cac04d95
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-01-15 08:16:19 +0700

    Implement Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,42 @@
+import ClickableComponent from '../ClickableComponent'
+
 export default function Footer() {
-  return <footer>Footer</footer>
+  return (
+    <footer className="mt-20">
+      <p className="bg-stone-200 pb-20 pt-5 text-center dark:bg-stone-200/10 md:pb-24">
+        This website was built with{' '}
+        <ClickableComponent
+          href="https://react.dev/"
+          target="_blank"
+          className="italic text-primary underline hover:underline md:no-underline"
+        >
+          React
+        </ClickableComponent>
+        ,{' '}
+        <ClickableComponent
+          href="https://www.typescriptlang.org/"
+          target="_blank"
+          className="italic text-primary underline hover:underline md:no-underline"
+        >
+          Typescript
+        </ClickableComponent>
+        , <br className="block md:hidden" />
+        <ClickableComponent
+          href="https://tailwindcss.com/"
+          target="_blank"
+          className="italic text-primary underline hover:underline md:no-underline"
+        >
+          Tailwindcss{' '}
+        </ClickableComponent>
+        & hosted on{' '}
+        <ClickableComponent
+          href="https://vercel.com/"
+          target="_blank"
+          className="italic text-primary underline hover:underline md:no-underline"
+        >
+          Vercel
+        </ClickableComponent>
+      </p>
+    </footer>
+  )
 }
